a1e28501e8d73490509c66c192df6c1981ace8b2
jj-o <101164155+jj-o@users.noreply.github.com>
2022-07-18 22:34:57 +0900

    Add files via upload

---
+++ 22.07.16~22.07.22(2주차)/문자열 압축/문자열 압축_권태은.py
@@ -0,0 +1,28 @@
+def solution(s):
+    length=[]
+    result=""
+
+    for piece in range(1,len(s)//2+1):
+        cnt=1
+        temp=s[:piece]
+        
+        for i in range(piece, len(s), piece):
+            if s[i:i+piece]==temp:
+                print(s[i])
+                print(s[i+piece])
+                cnt +=1
+            else:
+                if cnt==1:
+                    cnt=""
+                result += str(cnt)+temp
+                temp=s[i:i+piece]
+                cnt=1
+        
+        if cnt==1:
+            cnt=""
+        result += str(cnt)+temp
+        length.append(len(result))
+        result=""
+    return(min(length))  
+
+    
