7be9ab276e00e8930f001f6883d8eab91acbd89c
JunHyuk Kwon <97151660+tree-jhk@users.noreply.github.com>
2022-07-19 18:35:25 +0900

    Update 벽부수고이동하기_권준혁2.py

--- 22.07.09~22.07.15(1주차)/백준2206_벽부수고_이동하기/벽부수고이동하기_권준혁2.py
+++ 22.07.09~22.07.15(1주차)/백준2206_벽부수고_이동하기/벽부수고이동하기_권준혁2.py
@@ -21,11 +21,11 @@ def bfs() -> int:
             # 만약 xx,yy가 영역 내에 있다면
             if 0<=xx<n and 0<=yy<m:
                 # 만약 다음 칸이 벽이 아니라면
-                if board[xx][yy] == 0:
+                if board[xx][yy] == 0 and visited[xx][yy][crashed] == 0:
                     visited[xx][yy][crashed] = visited[x][y][crashed] + 1
                     q.append((xx,yy,crashed))
                 # 만약 다음 칸이 벽이라면
-                elif board[xx][yy] == 1:
+                elif board[xx][yy] == 1 and visited[xx][yy][crashed] == 0:
                     # crashed == 1: 이전에 벽을 부쉈기 때문에 벽을 부수지 않는다.
                     if crashed == 1:
                         continue
