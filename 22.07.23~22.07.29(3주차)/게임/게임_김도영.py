819aad7395d982ec335b1e7ae2a51d5ffd626e80
doyoung <66362265+rlaehdud1012@users.noreply.github.com>
2022-07-29 21:00:16 +0900

    dy

--- 22.07.23~22.07.29(3주차)/게임/게임_김도영.py
+++ 22.07.23~22.07.29(3주차)/게임/게임_김도영.py
@@ -1,3 +1,4 @@
+# 풀이1
 import sys
 input = sys.stdin.readline
 
@@ -20,3 +21,27 @@ else:
             right = mid - 1
             
     print(answer)
+    
+    
+# 풀이2
+x, y = map(int, input().split())
+z = (y * 100) // x
+
+def binary_search(z, start, end):
+    if z >= 99:
+        return -1
+    result = []
+    while start <= end:
+        mid = (start + end) // 2
+        new_z = ( (y + mid) * 100) // (x + mid)
+        if new_z > z:
+            result.append(mid)
+            end = mid - 1
+        else:
+            start = mid + 1
+    return result
+
+if binary_search(z, 1, x) == -1:
+    print(-1)
+else:
+    print(min(binary_search(z, 1, x)))
--- 22.07.23~22.07.29(3주차)/유기농 배추/유기농배추_김도영.py
+++ 22.07.23~22.07.29(3주차)/유기농 배추/유기농배추_김도영.py
@@ -1,3 +1,4 @@
+# 풀이1
 from collections import deque
 
 dx = [0,0,1,-1]
@@ -35,5 +36,50 @@ for i in range(t):
         for b in range(m):
             if graph[a][b] == 1:
                 bfs(graph, a, b)
+                cnt += 1
+    print(cnt)
+    
+    
+# 풀이2
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+# Test Case 수 T 입력
+t = int(input())
+
+# 상하좌우 이동
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+# x : 행, y : 열
+def dfs(graph, x, y):
+    graph[x][y] = 0
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
+        else:
+            if graph[nx][ny] == 1:
+                dfs(graph, nx, ny)
+    return
+
+for i in range(t):
+    # 가로 길이 M, 세로 길이 N, 배추가 심어져있는 위치의 개수 K 입력
+    m, n, k = map(int, input().split())
+    # 배추 graph 및 방문 여부 list 생성
+    graph = [ [0] * m for _ in range(n) ]
+
+    # 배추 위치값 입력받기
+    for i in range(k):
+        m_v, n_v = map(int, input().split())
+        graph[n_v][m_v] = 1
+
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                dfs(graph, i, j)
                 cnt += 1
     print(cnt)
